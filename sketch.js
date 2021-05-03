2f95c025561dd67238889094e183bf79054bf2c5
Fredy German <fredgerman30@gmail.com>
2021-05-03 22:36:48 +0300

    full screen mobile

--- sketch.js
+++ sketch.js
@@ -9,8 +9,8 @@ var highScore = localStorage.getItem("highScore");
 
 function setup() {
   // Creating canvas and initializing variables
-  //createCanvas(windowWidth-500, windowHeight-50);
-  createCanvas(400,800);
+  createCanvas(windowWidth-500, windowHeight-50);
+  //createCanvas(400,800);
   circle = new Circle();
   document.getElementById("level").innerHTML = `Current High Score is ${highScore}, Try to beat it`
 
